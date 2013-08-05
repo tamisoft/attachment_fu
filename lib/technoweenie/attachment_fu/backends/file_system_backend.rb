e71a540a97db72cb20ee01782ecec40ad001be7f
Levente Tamas <tamisoft@gmail.com>
2013-08-05 23:05:19 +0300

    Modified path to give more depth

--- lib/technoweenie/attachment_fu/backends/file_system_backend.rb
+++ lib/technoweenie/attachment_fu/backends/file_system_backend.rb
@@ -57,7 +57,7 @@ module Technoweenie # :nodoc:
             path_id = attachment_path_id
             if path_id.is_a?(Integer)
               # Primary key is an integer. Split it after padding it with 0.
-              ("%08d" % path_id).scan(/..../) + args
+              ("%012d" % path_id).scan(/..../) + args
             else
               # Primary key is a String. Hash it, then split it into 4 components.
               hash = Digest::SHA512.hexdigest(path_id.to_s)
